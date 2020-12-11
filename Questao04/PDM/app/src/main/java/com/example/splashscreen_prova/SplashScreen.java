39a38ab3c6e3995c85b6894189975b191d346133
emanuelConte <36667290+emanuelconte@users.noreply.github.com>
2020-12-10 22:13:12 -0300

    Add files via upload

---
+++ Questao04/PDM/app/src/main/java/com/example/splashscreen_prova/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.splashscreen_prova;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    private static int SPLASH_TEMPO_LIMITE = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this,
+                        MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TEMPO_LIMITE);
+    }
+}
+
